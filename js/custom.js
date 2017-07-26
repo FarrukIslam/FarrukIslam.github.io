753ab42b3ea62d88f91a0a951657e1844dee1029
FarrukIslam <farrukislam1@gmail.com>
2017-07-26 23:48:43 +0600

    add portfolio section

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+(function($) {
+	$(function() { //on DOM ready
+		$("#scroller").simplyScroll({
+			customClass: 'vert',
+			orientation: 'vertical',
+            auto: false,
+            manualMode: 'loop',
+			frameRate: 20,
+			speed: 25
+		});
+	});
+})(jQuery);
